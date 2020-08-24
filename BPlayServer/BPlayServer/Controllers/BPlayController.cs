d7de880ef242106eedcebcb6ddaf81bec130ecc4
Belchuke <34919402+Belchuke@users.noreply.github.com>
2020-08-24 14:50:56 +0200

    Added examples for post and get

---
+++ BPlayServer/BPlayServer/Controllers/BPlayController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BPlayServer.Data;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BPlayServer.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    [EnableCors]
+    public class BPlayController : ControllerBase
+    {
+
+        /// Get example
+        [EnableCors]
+        [HttpGet("/getstringexample")] // url path --- localhost:5000/api/getstringexample
+        public ActionResult<string> ReturnRandomStringAsJson()
+        {
+            string randomString = "Return this string over API";
+            return Ok(randomString);
+        }
+
+        /// Post example 
+        [EnableCors]
+        [HttpPost("/Ppostexample")]
+        public IActionResult GetStringAndReturnANewString([FromBody] string result)
+        {
+            return Ok(result + "this returns");
+        }
+    }
+}
